d848b31fe965b1341b62000008c0be06218343ba
Joaquin <joagrosso@hotmail.com>
2022-05-26 11:13:20 -0300

    Fin seccion pipes

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -45,4 +46,26 @@ export class NoComunesComponent  {
       direccion : "La Plata, Buenos Aires"
     };
 
+
+    heroes = [
+      {
+        nombre : 'Superman',
+        vuela : true
+      },
+      {
+        nombre : 'Robin',
+        vuela : false
+      },
+      { 
+        nombre : 'Aquaman',
+        vuela : false
+      }
+    ];
+
+    myObservable = interval(1000);
+    valorPromesa = new Promise((resolve,reject) => {
+      setTimeout(() => {
+        resolve('Tenemos data de la promesa');
+      },3500);
+    });
 }
