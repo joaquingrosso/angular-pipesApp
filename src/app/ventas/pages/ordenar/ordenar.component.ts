6b7a9a9b60492e28457f67eb9d3805aa0be9d40f
Joaquin <joagrosso@hotmail.com>
2022-06-07 11:47:09 -0300

    final de pipes

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -6,11 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
+  enMayus: boolean = true;
+
+  heroes: Heroe[] = [
+    {
+      nombre: 'Superman',
+      vuela: true, 
+      color: 0
+    },
+    {
+      nombre: 'Batman',
+      vuela: false, 
+      color: 1
+    },
+    {
+      nombre: 'Linterna Verde',
+      vuela: true, 
+      color: 3
+    },
+    {
+      nombre: 'Spiderman',
+      vuela: false, 
+      color: 0
+    },
+    {
+      nombre: 'Aquaman',
+      vuela: false, 
+      color: 2
+    }
+  ];
+
+  ordenarPor: string = 'sin valor';
+
+  cambiarMayusculas(){
+    return ( this.enMayus ) ? this.enMayus=false : this.enMayus=true;
+  };
+
+  cambiarOrden(orden: string){
+    this.ordenarPor = orden;
   }
 
 }
